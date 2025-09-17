08fdba09dcbc8619e34d44f794e074b57610e79a
yvessrib <yves@gec.inatel.br>
2025-09-16 22:07:26 -0300

    feat: prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">Submit</button>
     </div>
   )
 }
